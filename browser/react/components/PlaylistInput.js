3ce4df79a8c35d1a0315aaf9e0122ce34441b09f
Lada Tochtcheva <lada.toch@gmail.com>
2017-03-30 12:21:45 -0400

    validating

--- browser/react/components/PlaylistInput.js
+++ browser/react/components/PlaylistInput.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const PlaylistInput = (props) => {
+
+  //className = "ggghh gghhjh disabled"
   return (
     <div className="well">
       <form className="form-horizontal" onSubmit={props.onSubmit}>
@@ -9,12 +11,12 @@ const PlaylistInput = (props) => {
           <div className="form-group">
             <label className="col-xs-2 control-label">Name</label>
             <div className="col-xs-10">
-              <input onChange={props.onChange} className="form-control" type="text" />
+              <input onChange={props.onChange} className="form-control" type="text" value={props.inputValue} />
             </div>
           </div>
           <div className="form-group">
             <div className="col-xs-10 col-xs-offset-2">
-              <button type="submit" className="btn btn-success">Create Playlist</button>
+              <button type="submit" className="btn btn-success" {5 ===5 ?className ="someth" : ''}  >Create Playlist</button>
             </div>
           </div>
         </fieldset>
--- browser/react/containers/NewPlaylist.js
+++ browser/react/containers/NewPlaylist.js
@@ -5,7 +5,8 @@ class NewPlaylist extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      inputValue: ''
+      inputValue: '',
+      validInput: true
     }
     this.submitPlaylist = this.submitPlaylist.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -19,17 +20,29 @@ class NewPlaylist extends React.Component {
     this.setState({
       inputValue : event.target.value
     })
+    this.validate(this.state.inputValue)
+  }
+//function validate(input val) => true/false isValid
+  validate (input) {
+    if (input.length <= 16 && input.length > 0 ) this.setState({
+      validInput: true})
+    else this.setState({
+      validInput: false
+    })
   }
 
   submitPlaylist(event) {
-    console.log(this.state.inputValue)
+    console.log(this.state.inputVlue)
     event.preventDefault();
+    this.setState({
+      inputValue : ""
+    })
   }
 
   render() {
     return (
         <div>
-          <PlaylistInput onChange={this.handleChange} onSubmit={this.submitPlaylist} />
+          <PlaylistInput onChange={this.handleChange} onSubmit={this.submitPlaylist} inputValue={this.state.inputValue} isValid={this.state.validInput} />
         </div>
       );
   }
